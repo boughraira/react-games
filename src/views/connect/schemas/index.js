9e023fa3cf46c9ef4917f30e07f6837863bc1d74
Selmen Rayen <selmanerayene95@gmail.com>
2023-09-06 05:04:31 +0100

    fix validation schema

--- src/views/connect/schemas/index.js
+++ src/views/connect/schemas/index.js
@@ -3,27 +3,29 @@ import * as yup from "yup";
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 
 export const basicSchema = yup.object().shape({
-  email: yup.string().email("Please enter a valid email"),
+  email: yup.string().email("Please enter a valid email").required("Required"),
   age: yup.number().positive().integer().required("Required"),
   password: yup
     .string()
     .min(5)
     .matches(passwordRules, { message: "Please create a stronger password" })
-    .required('Rquired'),
-    confirmPassword:yup.string().oneOf([yup.ref('password'),null],'Passwords must match')
-    .required('Rquired')
+    .required("Rquired"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("Rquired"),
 });
 
 export const advancedSchema = yup.object().shape({
-    username: yup
-      .string()
-      .min(3, "Username must be at least 3 characters long")
-      .required("Required"),
-    jobType: yup
-      .string()
-      .oneOf(["designer", "developer", "manager", "other"], "Invalid Job Type")
-      .required("Required"),
-    acceptedTos: yup
-      .boolean()
-      .oneOf([true], "Please accept the terms of service"),
-  });
+  username: yup
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .required("Required"),
+  jobType: yup
+    .string()
+    .oneOf(["designer", "developer", "manager", "other"], "Invalid Job Type")
+    .required("Required"),
+  acceptedTos: yup
+    .boolean()
+    .oneOf([true], "Please accept the terms of service"),
+});
